314646ad64a08335963207201b8ffa7b2a72882b
sup25 <suparnaa51@gmail.com>
2023-02-16 23:06:57 +0545

    updated

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import { Add } from "@mui/icons-material";
+import { useList } from "@pankod/refine-core";
+import {
+    Box,
+    Stack,
+    Typography,
+    TextField,
+    Select,
+    MenuItem,
+} from "@pankod/refine-mui";
+import { useNavigate } from "@pankod/refine-react-router-v6";
+import { useMemo } from "react";
 
 const allProperties = () => {
     return (
