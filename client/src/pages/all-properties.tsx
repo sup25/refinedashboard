ddb664aa76893784fcb0cef9ca3393444ede67fa
sup25 <suparnaa51@gmail.com>
2023-02-24 21:00:01 +0545

    updated

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -16,7 +16,13 @@ import { PropertyCard, CustomButton } from "components";
 const AllProperties = () => {
     const navigate = useNavigate();
     const {
-        tableQueryResult: { data, isLoading, isError }
+        tableQueryResult: { data, isLoading, isError },
+        current,
+        setCurrent,
+        setPageSize,
+        pageCount,
+        sorter, setSorter,
+        filters, setFilters,
     } = useTable();
 
     const allProperties = data?.data ?? []
@@ -25,8 +31,45 @@ const AllProperties = () => {
     if (isError) return <Typography>Error...</Typography>
     return (
         <Box>
+            <Box mt="20px" sx={{ display: 'flex', flexWrap: "wrap", gap: 3 }}>
+                <Stack direction="column" width="100%">
+                    <Typography fontSize={25} fontWeight={700} color="#11142d">
+                        {!allProperties.length ? 'there are no properties' : "All Properties"}
+                    </Typography>
+                    <Box mb={2} mt={3} display="flex" width="84%" justifyContent="space-between" flexWrap="wrap">
+                        <Box display="flex" gap={2} flexWrap="wrap" mb={{ xs: '20px', sm: 0 }}>
+                            <CustomButton
+                                title={`Sort price`}
+                                handleClick={() => { }}
+                                backgroundColor='#475be8'
+                                color="#fcfcfc"
+                            />
+                            <TextField
+                                variant="outlined"
+                                color="info"
+                                placeholder="search by title"
+                                value=''
+                                onChange={() => { }}
+                            />
+                            <Select
+                                variant="outlined"
+                                color="info"
+                                displayEmpty
+                                required
+                                inputProps={{ 'aria-label': 'Without label' }}
+                                defaultValue=""
+                                value=""
+                                onChange={() => { }}
+                            >
+                                <MenuItem value="">All</MenuItem>
+                            </Select>
+
+                        </Box>
+                    </Box>
+                </Stack>
+            </Box>
             <Stack direction="row" justifyContent="space-between" alignItems="center">
-                <Typography fontSize={25} fontWeight={700} color="#11142d">All Properties</Typography>
+
                 <CustomButton
                     title="Add Property"
                     handleClick={() => navigate('/properties/create')}
@@ -49,6 +92,21 @@ const AllProperties = () => {
 
                 ))}
             </Box>
+            {allProperties.length > 0 && (
+                <Box display="flex" gap={2} mt={3} flexWrap="wrap">
+                    <CustomButton
+                        title="Previous"
+                        handleClick={() => setCurrent((prev) => prev - 1)}
+                        backgroundColor="#475be8"
+                        color="#fcfcfc"
+                        disabled={!(current > 1)}
+
+                    />
+                    <Box display={{ xs: 'hidden', sm: 'flex' }} alignItems="center" gap="5px">
+                        Page  {''}<strong>{current} of {pageCount}</strong>
+                    </Box>
+                </Box>
+            )}
         </Box>
     )
 }
